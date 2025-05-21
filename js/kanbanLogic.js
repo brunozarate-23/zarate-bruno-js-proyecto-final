b2988d0289e8a47992917ec6c17d56f0f5f7bfad
Bruno Zarate <zratebruno@gmail.com>
2025-05-21 13:02:44 -0300

    Commit para continuar en otra PC

---
+++ js/kanbanLogic.js
@@ -0,0 +1,8 @@
+// Cargar contenido del DOM primero
+document.addEventListener('DOMContentLoaded', function() {
+    const nuevoClienteColumna = document.getElementById('newClientKanban');
+    const interesadoColumna = document.getElementById('interesadoKanban');
+    const concretadoColumna = document.getElementById('concretadoKanban');
+    const perdidoColumna = document.getElementById('perdidoKanban');
+});
+

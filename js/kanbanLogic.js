807867fd2204f13d59707cc7ff47edc00e802dbb
Bruno Zarate <zratebruno@gmail.com>
2025-05-21 20:03:08 -0300

    logica de tarjetas kanban creada

--- js/kanbanLogic.js
+++ js/kanbanLogic.js
@@ -6,3 +6,56 @@ document.addEventListener('DOMContentLoaded', function() {
     const perdidoColumna = document.getElementById('perdidoKanban');
 });
 
+// Traemos la data de Contacts.json con un fetch
+fetch('../database/contacts.json')
+    .then(response => response.json())
+    .then(data => {
+        // Busco recorrer toda la data para crear una tarjeta en su columna
+        data.forEach(lead => {
+            // Creo un div con clase de Bootstrap
+            const card = document.createElement('div');
+            card.className = 'card mb-2 border-dark';
+            // Creo el body de la card
+            const cardBody = document.createElement('div');
+            cardBody.className ='card-body';
+            // Creo el titulo de la card
+            const nombre = document.createElement('h5');
+            nombre.className = 'card-title text-center';
+            nombre.textContent = lead.nombre;
+            // Creo una lista con cada uno de los componentes del lead
+            const listGroup = document.createElement('ul');
+            const telefono = document.createElement('li');
+            const email = document.createElement('li');
+            const direccion = document.createElement('li');
+            const productoInteresado = document.createElement('li');
+            const precioProducto = document.createElement('li');
+            // Le agrego las clases de Bootstrap
+            listGroup.className = 'list-group list-group-flush';
+            telefono.className = 'list-group-item';
+            email.className = 'list-group-item';
+            direccion.className = 'list-group-item';
+            productoInteresado.className = 'list-group-item';
+            precioProducto.className = 'list-group-item';
+            // Agrego el texto de cada lead
+            telefono.textContent = 'Télefono: ' + lead.telefono;
+            email.textContent = 'Email: ' + lead.email;
+            direccion.textContent = 'Dirección: ' + lead.direccion;
+            productoInteresado.textContent = 'Interés: ' + lead.producto_interesado;
+            precioProducto.innerHTML = 'Valor: ' + '$' + lead.precio_producto;
+            // Se arma la estructura de la carta
+            card.appendChild(cardBody);
+            cardBody.appendChild(nombre);
+            cardBody.appendChild(listGroup);
+            listGroup.appendChild(telefono);
+            listGroup.appendChild(email);
+            listGroup.appendChild(direccion);
+            listGroup.appendChild(productoInteresado);
+            listGroup.appendChild(precioProducto);
+
+            // Agregar a la columna correcta segun su estado en el Pipeline de Ventas
+            const columna = document.getElementById(lead.estado_lead);
+            if (columna) {
+                columna.appendChild(card);
+            }
+        });
+    })
